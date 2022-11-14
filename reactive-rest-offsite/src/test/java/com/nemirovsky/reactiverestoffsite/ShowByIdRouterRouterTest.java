6156b8c5ef96ade272929d64f928ac908b462e2b
Stanislav Nemirovsky <choonsky@gmail.com>
2022-11-14 18:55:40 +0600

    Added Router Test

---
+++ reactive-rest-offsite/src/test/java/com/nemirovsky/reactiverestoffsite/ShowByIdRouterRouterTest.java
@@ -0,0 +1,33 @@
+package com.nemirovsky.reactiverestoffsite;
+
+import com.nemirovsky.reactiverestoffsite.dto.SimpleResponce;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ShowByIdRouterRouterTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testHello() {
+        webTestClient
+                // Create a GET request to test an endpoint
+                .get().uri("/54321")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                // and use the dedicated DSL to test assertions against the response
+                .expectStatus().isOk()
+                .expectBody(SimpleResponce.class).value(greeting -> {
+                    assertThat(greeting.getMessage()).isEqualTo("TEST MESSAGE");
+                });
+    }
+}
